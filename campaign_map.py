b46bdfdfb4059422f67fe3d1d7a686f6b5117697
jghibiki <jghibiki.games@gmail.com>
2016-11-17 13:23:13 -0600

    rename region to map

--- campaign_map.py
+++ campaign_map.py
@@ -8,22 +8,21 @@ locale.setlocale(locale.LC_ALL, '')
 
 
 
-@click.group()
-
+@click.group("map")
 @click.pass_context
-def region(ctx):
+def campaign_map(ctx):
     pass
 
 @click.command("add")
 @click.option("--max-x", default=100, type=int)
 @click.option("--max-y", default=100, type=int)
-@click.argument("region_name")
+@click.argument("map_name")
 @click.pass_context
-def add_region(ctx, max_x, max_y, region_name):
+def add_map(ctx, max_x, max_y, map_name):
     campaign_obj = load(ctx)
 
 
-    campaign_obj["regions"][region_name] = {
+    campaign_obj["maps"][map_name] = {
         "max_x": max_x,
         "max_y": max_y,
         "features": []
@@ -32,11 +31,11 @@ def add_region(ctx, max_x, max_y, region_name):
     save(ctx, campaign_obj)
 
 @click.command("rm")
-@click.argument("region_name")
+@click.argument("map_name")
 @click.pass_context
-def remove_region(ctx, region_name):
+def remove_map(ctx, map_name):
     campaign_obj = load(ctx)
-    del campaign_obj["regions"][region_name]
+    del campaign_obj["maps"][map_name]
     save(ctx, campaign_obj)
 
 @click.command("list")
@@ -45,18 +44,18 @@ def list(ctx):
     campaign_obj = load(ctx)
     intro = "%s" % campaign_obj["title"]
     pad = len(intro)*"="
-    print("%s\n%s\n%s\nRegions:" % (pad, intro, pad))
-    for region in campaign_obj["regions"].keys():
-        print("|-> %s" % region)
+    print("%s\n%s\n%s\nMaps:" % (pad, intro, pad))
+    for cmap in campaign_obj["maps"].keys():
+        print("|-> %s" % cmap)
 
 
 @click.command("edit")
-@click.argument("region_name")
+@click.argument("map_name")
 @click.pass_context
-def edit_region(ctx, region_name):
-    curses.wrapper(_edit_region, ctx, region_name)
+def edit_map(ctx, map_name):
+    curses.wrapper(_edit_map, ctx, map_name)
 
-def _edit_region(scr, ctx, region_name):
+def _edit_map(scr, ctx, map_name):
     campaign_obj = load(ctx)
 
     curses.start_color()
@@ -65,8 +64,8 @@ def _edit_region(scr, ctx, region_name):
         curses.init_pair(i + 1, i, -1)
 
     init()
-    map = Map(campaign_obj["regions"][region_name])
-    editor = Editor(scr, map, save_region(ctx, region_name, campaign_obj))
+    campaign_map = Map(campaign_obj["maps"][map_name])
+    editor = Editor(scr, campaign_map, save_map(ctx, map_name, campaign_obj))
 
     zz = False
     while True:
@@ -74,16 +73,16 @@ def _edit_region(scr, ctx, region_name):
         editor.handle(ch)
         editor.draw()
 
-def save_region(ctx, region, campaign_obj):
-    def _save_region_callback(region_object):
-        campaign_obj["regions"][region] = region_object
+def save_map(ctx, cmap, campaign_obj):
+    def _save_map_callback(map_object):
+        campaign_obj["campaign_maps"][cmap] = map_object
         save(ctx, campaign_obj)
-    return _save_region_callback
+    return _save_map_callback
 
 
-region.add_command(add_region)
-region.add_command(remove_region)
-region.add_command(edit_region)
-region.add_command(list)
+campaign_map.add_command(add_map)
+campaign_map.add_command(remove_map)
+campaign_map.add_command(edit_map)
+campaign_map.add_command(list)
 
 
