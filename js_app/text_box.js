aaea411e5abed8825afba85a7015c24834172fd9
Travis CI <jghibiki.games@gmail.com>
2017-05-31 02:52:15 +0000

    Deploy to GitHub Pages: 88c6e31c7efccad6489d91b3f3bf2cfa16a975a4

--- js_app/text_box.js
+++ js_app/text_box.js
@@ -135,6 +135,8 @@ text_box.init = function(){
     ];
 
     text_box.text = text_box.default_lines;
+    text_box.rendered_text = null;
+    text_box.handling_page = false;
     cursed.modules.interactive.push(text_box);
 };
 text_box.draw = function(){
@@ -188,7 +190,7 @@ text_box.draw = function(){
                         0, 
                         cursed.constants.font_size + cursed.constants.font_width_offset, 
                         text_box.height));
-                
+               
                 box.graphics.append(new createjs.Graphics.Fill(cursed.colors.get("Gold").value));
 
                 cursed.stage.addChild(box);
@@ -196,102 +198,45 @@ text_box.draw = function(){
 
             cursed.stage.addChild(text_box.text_container);
 
+
             text_box.first_draw = false;
         }
         
         /* draw text */
         text_box.text_container.removeAllChildren();
-        var line_no = 0;
-        var line_width = text_box.width  - (cursed.constants.font_size * 3)
 
-        var paged_text = text_box.text.slice(text_box.page, text_box.text.length);
-        for(var line of paged_text){
-            var ch = 0; //number of characters in line so far
-            
-            for(var part of line){
-                var color_obj = null
-                if(part.hasOwnProperty("color") && part.color !== null){
-                    color_obj = cursed.colors.get(part.color);
-                }
-                else{
-                    color_obj = cursed.colors.get("White");
-                }
-
-
-                var buff = part.text;
-                var overflow_buff = ""
-
-                while(buff.length > 0 || overflow_buff > 0){
-
-                
-                    var text = new createjs.BitmapText(buff, cursed.ss);
-                    text.letterSpacing = cursed.constants.font_spacing;
-
-                    while(text.getBounds().width + ch >= line_width && buff.length > 1){
-                        var tmp = buff.substring(0, buff.length-1);
-                        overflow_buff = buff.substring(buff.length-1, buff.length) + overflow_buff;
-                        buff = tmp;
-                        text = new createjs.BitmapText(buff, cursed.ss);
-                        text.letterSpacing = cursed.constants.font_spacing;
-                    }
-
-                    if(buff.length > 1){
-
-                        text.x = ch;
-                        text.y = (line_no * cursed.constants.font_size);
-                        text.letterSpacing = cursed.constants.font_spacing;
-                        text.lineHeight = cursed.constants.font_size*2;
-
-                        text.filters = [
-                            new createjs.ColorFilter(0, 0, 0, 1, color_obj.r, color_obj.g, color_obj.b, 0)
-                        ];
-
-                        var bounds = text.getBounds();
-                        var w = bounds.width;
-                        var h = bounds.height;
-                        text.cache(0, 0, w*2, h*2);
-                        text_box.text_container.addChild(text);
-
-                        ch += w;
-
-                        buff = overflow_buff;
-                        overflow_buff = "";
-
-                    }
-                    else if(buff.length === 1 && overflow_buff.length == 0){ //handles the case of only one character to print
-                        text.x = ch;
-                        text.y = (line_no * cursed.constants.font_size);
-                        text.letterSpacing = cursed.constants.font_spacing;
-                        text.lineHeight = cursed.constants.font_size*2;
-
-                        text.filters = [
-                            new createjs.ColorFilter(0, 0, 0, 1, color_obj.r, color_obj.g, color_obj.b, 0)
-                        ];
-
-                        var bounds = text.getBounds();
-                        var w = bounds.width;
-                        var h = bounds.height;
-                        text.cache(0, 0, w*2, h*2);
-                        text_box.text_container.addChild(text);
-
-                        ch += w - 4;
-
-                        buff = overflow_buff;
-                        overflow_buff = "";
-                    }
-                    else{
-                        ch = 0;
-                        line_no += 1;
-                        buff = buff + overflow_buff;
-                        overflow_buff = "";
-                    }
-                }
-
-            }
-
-            ch = 0;
-            line_no += 1;
+        if(text_box.rendered_text === null){
+            text_box.render_text();
         }
+
+        var max_entry = 50; //TODO make rows to render scale
+        if(max_entry > text_box.text.length){
+            max_entry = text_box.text.length;
+            text_box.page = text_box.page - 1 >= 0? text_box.page-1: text_box.page;
+        }
+
+        var page_counter = 0;
+        var line_no = 0;
+        for(var i=0; i<text_box.rendered_text.length; i++){
+            var line = text_box.rendered_text[i];
+            if(page_counter < text_box.page || page_counter > max_entry+text_box.page){
+                line_no = 0;
+                page_counter += 1
+                continue;
+            } 
+            else{
+                for(var line_part of line){
+
+                    line_part.y = (line_no * cursed.constants.font_size);
+                    text_box.text_container.addChild(line_part);
+
+                }
+                page_counter += 1;
+                line_no += 1;
+            }
+        }
+
+        text_box.handling_page = false;
 
 		//text_box.text_container.updateCache();
         text_box.dirty = false;
@@ -300,6 +245,7 @@ text_box.draw = function(){
 }
 
 text_box.set = function(text){
+    text_box.rendered_text = null;
     text_box.text = text;
     text_box.dirty = true;
     text_box.page = 0;
@@ -308,18 +254,28 @@ text_box.set = function(text){
 
 text_box.handle = function(e){
 
-    if(e.key === "PageDown"){
-        text_box.page += 1;
-        text_box.dirty = true;
-        text_box.draw();
-    }
+    if(! text_box.handling_page){
+        if(e.key === "PageDown"){
+            text_box.handling_page = true;
+            text_box.page += 10;
+            text_box.dirty = true;
+            text_box.draw();
+        }
 
-    if(e.key === "PageUp"){
-		if(text_box.page > 0){
-			text_box.page -= 1;
-			text_box.dirty = true;
-			text_box.draw();
-		}
+        if(e.key === "PageUp"){
+            if(text_box.page - 10 >= 0){
+                text_box.handling_page = true;
+                text_box.page -= 10;
+                text_box.dirty = true;
+                text_box.draw();
+            }
+            else if(text_box.page - 1 >= 0){
+                text_box.handling_page = true;
+                text_box.page -= 1;
+                text_box.dirty = true;
+                text_box.draw();
+            }
+        }
     }
 
 }
@@ -333,4 +289,141 @@ text_box.handle_combo = function(buff){
     if(buff == "clear"){
         text_box.set(text_box.default_lines);
     }
+}
+
+text_box.render_text = function(){
+    console.log("rendering text")
+    var line_no = 0;
+    var line_width = text_box.width  - (cursed.constants.font_size * 3)
+
+    var rendered_lines = [];
+    for(var line of text_box.text){
+        var ch = 0; //number of characters in line so far
+
+        var broken = false;
+        
+        //TODO fix issue with not paging wrapped lines
+        //TODO fix issue with missing letter when wrapping line
+
+        var current_line = [];
+        part_loop: for(var part of line){
+            //if(page_counter < text_box.page || page_counter > max_entry+text_box.page){
+            //    line_no = 0;
+            //    page_counter += 1
+            //    broken = true;
+            //    break;
+            //}
+
+            var color_obj = null
+            if(part.hasOwnProperty("color") && part.color !== null){
+                color_obj = cursed.colors.get(part.color);
+            }
+            else{
+                color_obj = cursed.colors.get("White");
+            }
+
+
+            if(part.text.length > 0){
+                var overflow_buff = part.text.replace(/[\n\r]+/g, '');
+            }
+            else{ var overflow_buff = ""; }
+            var buff = overflow_buff[0];
+            overflow_buff = overflow_buff.slice(1, overflow_buff.length);
+
+
+            buff_loop: while(buff !== undefined && (buff.length > 0 || overflow_buff.length > 0)){
+                var text = new createjs.BitmapText(buff, cursed.ss);
+                text.letterSpacing = cursed.constants.font_spacing;
+
+                while(text.getBounds().width + ch < line_width && overflow_buff.length > 0){
+                    var tmp = overflow_buff[0];
+                    overflow_buff = overflow_buff.slice(1, overflow_buff.length);
+                    buff = buff + tmp;
+                    text = new createjs.BitmapText(buff, cursed.ss);
+                    text.letterSpacing = cursed.constants.font_spacing;
+                }
+
+
+                if(buff.length > 1){
+
+                    text.x = ch;
+
+                    text.filters = [
+                        new createjs.ColorFilter(0, 0, 0, 1, color_obj.r, color_obj.g, color_obj.b, 0)
+                    ];
+
+                    var bounds = text.getBounds();
+                    var w = bounds.width;
+                    var h = bounds.height;
+                    //text_box.text_container.addChild(text);
+                    current_line.push(text);
+
+                    ch += w;
+
+                    if (overflow_buff.length > 0){
+                        buff = overflow_buff[0];
+                        overflow_buff = overflow_buff.slice(1, overflow_buff.length);
+                    }
+                    else{ 
+                        buff = "";
+                        continue; 
+                    }
+                }
+                if(buff.length == 1 && overflow_buff.length == 0){
+                    ch = 0;
+
+                    text.x = ch;
+
+                    text.filters = [
+                        new createjs.ColorFilter(0, 0, 0, 1, color_obj.r, color_obj.g, color_obj.b, 0)
+                    ];
+
+                    var bounds = text.getBounds();
+                    var w = bounds.width;
+                    var h = bounds.height;
+                    //text_box.text_container.addChild(text);
+                    current_line.push(text);
+                    rendered_lines.push(current_line);
+                    current_line = [];
+
+                    ch += w;
+
+                    var buff = overflow_buff[0];
+                    overflow_buff = overflow_buff.slice(1, overflow_buff.length);
+
+                }
+                else{
+                    ch = 0;
+                    //var buff = overflow_buff[0];
+                    //overflow_buff = overflow_buff.slice(1, overflow_buff.length);
+
+                    rendered_lines.push(current_line);
+                    current_line = [];
+                }
+
+            }
+        }
+
+
+        ch = 0;
+        rendered_lines.push(current_line);
+        current_line = [];
+    }
+
+    // update cache for all lines
+    for(var i=0; i<rendered_lines.length; i++){
+        var line = rendered_lines[i];
+        for(var line_part of line){
+
+            line_part.letterSpacing = cursed.constants.font_spacing;
+            line_part.lineHeight = cursed.constants.font_size*2;
+
+            var bounds = line_part.getBounds();
+            var w = bounds.width;
+            var h = bounds.height;
+            line_part.cache(0, 0, w*2, h*2);
+        }
+    }
+
+    text_box.rendered_text = rendered_lines;
 }
